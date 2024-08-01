e2a210c95f172dc9a9dfc491f74ce09c438296a8
Naina Johri <jhorinaina8@gmail.com>
2024-08-01 21:31:36 +0530

    .env file created and server.js file updated

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const experss = require("express");
+const app = experss();
+require('dontenv').config()
 
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
